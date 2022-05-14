7e56660ea6ac510d7eecb709ccc691b023c8208d
Matheus Martins <matheus.martins2198@gmail.com>
2022-05-13 23:33:48 -0300

    feat: productDisplay and products list layout

--- src/pages/Products/ProductDisplay.tsx
+++ src/pages/Products/ProductDisplay.tsx
@@ -4,11 +4,12 @@ import {
 	Box,
 	Flex,
 	Image,
+	Heading,
 	Text,
 	Input,
 	Button
 } from '@chakra-ui/react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 function ProductDisplay() {
 	const { uuid } = useParams();
@@ -17,24 +18,55 @@ function ProductDisplay() {
 		<>
 			<Grid 
 				p='10'
-				templateColumns='repeat(5, 1fr)'
-				gap={5}
+				templateColumns='repeat(10, 1fr)'
+				gap={10}
 			>
-				<GridItem colSpan={3}>
+				<GridItem colSpan={6}>
 					<Box border='1px' borderRadius='.5rem' borderColor='blackAlpha.300'>
 						<Flex flexDirection='column'>
 							<Box borderBottom='1px' borderColor='blackAlpha.300'>
-								<Image p='4rem' display='block' mr='auto' ml='auto' src='https://images.lojanike.com.br/1024x1024/produto/bola-futebol-americano-nike-vapor-247-oficial-FT0268-211-1.png' />
+								<Image p='4rem' width='30rem' display='block' mr='auto' ml='auto' src='https://d3ugyf2ht6aenh.cloudfront.net/stores/862/279/products/thumb-3161-07ca21906c145bc09115712480190573-1024-1024.jpg' />
 							</Box>
-							<Box>
-								
+							<Box alignItems='left' borderBottom='1px' borderColor='blackAlpha.300'>
+								<Heading p="6" size='sm' color='blackAlpha.900'>DESCRIÇÃO</Heading>
+							</Box>
+							<Box alignItems='left'>
+								<Text p="8" color='blackAlpha.800'>
+									Uniforme Cincinnati Bengals 100% Poliester
+								</Text>
 							</Box>
 						</Flex>
 					</Box>
 				</GridItem>
-				<GridItem colSpan={2}>
+				<GridItem colSpan={4}>
 					<Box border='1px' borderRadius='.5rem' borderColor='blackAlpha.300'>
-						
+						<Flex flexDirection='column'>
+							<Box alignItems='left' borderBottom='1px' borderColor='blackAlpha.300'>
+								<Text p="6" fontSize='1.8rem' color="blackAlpha.700">Uniforme Cincinnati Bengals</Text>
+							</Box>
+							<Box alignItems='left' borderBottom='1px' borderColor='blackAlpha.300'>
+								<Box p="6">
+									<Text color='blackAlpha.700' fontSize='xl'>De 5000 pontos</Text>
+									<Text color='gray.500' fontSize='2xl'>Por 4000 pontos </Text>
+									<Button mt="6" display='block' mr='auto' ml='auto' bgColor='green.300' size='lg' color='white' _hover={{ bgColor: 'green.400' }} _focus={{ border: 'none' }} _active={{ bgColor: 'green.500' }} >
+										ADICIONAR AO CARRINHO
+									</Button>
+								</Box>
+							</Box>
+							<Box alignItems='left' borderBottom='1px' borderColor='blackAlpha.300'>
+							<Box p="6">
+									<Text color='blue.400' fontSize='md'>FRETE E PRAZO DE ENTREGA</Text>
+									<Text mt='2' color='gray.500' fontSize='sm'>Calcule o valor do seu frete e saiba o prazo de entrega.</Text>
+									<Flex mt='3' alignItems='center' justifyContent='space-between'>
+										<Input placeholder='CEP' width='50%' />
+										<Button size='md' color='black' variant='outline'>
+											CALCULAR
+										</Button>
+										<Link to='#'>Procurar CEP</Link>
+									</Flex>
+								</Box>
+							</Box>
+						</Flex>
 					</Box>
 				</GridItem>
 			</Grid>
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,6 +1,65 @@
+import { 
+	Box, 
+	Button, 
+	Flex, 
+	Heading, 
+	Input,
+	Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+	ButtonGroup,
+} from "@chakra-ui/react";
+
 function Products() {	
 	return (
-		<h1>Products</h1>
+		<Box p='10'>
+			<Heading size='lg' color='blackAlpha.900'>Lista de Produtos</Heading>
+			<Flex width='50rem' flexWrap='wrap' mt='3' alignItems='center' justifyContent='space-between'>
+				<Input width='10rem' placeholder='Título' />
+				<Input width='10rem' placeholder='Descrição' />
+				<Input width='10rem' placeholder='Valor' />
+				<Input width='10rem' placeholder='Desconto' />
+				<Button bgColor='green.300' size='md' color='white' _hover={{ bgColor: 'green.400' }} _focus={{ border: 'none' }} _active={{ bgColor: 'green.500' }}>
+					ADICIONAR
+				</Button>
+			</Flex>
+			<Box p='6'>
+			<TableContainer>
+				<Table variant='simple'>
+					<TableCaption>Produtos cadastrados</TableCaption>
+					<Thead>
+						<Tr>
+							<Th>Título</Th>
+							<Th>Descrição</Th>
+							<Th isNumeric>Valor</Th>
+							<Th isNumeric>Desconto</Th>
+							<Th>Ações</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						<Tr>
+							<Td>Uniforme Cincinnati Bengals</Td>
+							<Td>Uniforme Cincinnati Bengals 100% Poliester</Td>
+							<Td isNumeric>5000</Td>
+							<Td isNumeric>10</Td>
+							<Td>
+								<ButtonGroup spacing='6'>
+									<Button colorScheme='yellow'>Editar</Button>
+									<Button colorScheme='red'>Excluir</Button>
+								</ButtonGroup>
+							</Td>
+						</Tr>
+					</Tbody>
+				</Table>
+			</TableContainer>
+			</Box>
+		</Box>
 	)
 }
 
